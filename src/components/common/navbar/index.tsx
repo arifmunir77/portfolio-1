36d6339141cac4014ea948cb7c092cd26c42a35a
Dev-SardarAbdul <dev.sardarabdul@gmail.com>
2023-05-28 19:24:18 +0500

    Hamburger anomations added

--- src/components/common/navbar/index.tsx
+++ src/components/common/navbar/index.tsx
@@ -25,6 +25,18 @@ function Topbar() {
           </VideoContainer>
           <IconHolderDiv
             onClick={() => setNavCollapsed(!navCollapsed)}
+            animate={{
+              scale: [1, 1.2, 1.2, 1, 1],
+              rotate: [0, 0, 180, 180, 0],
+              borderRadius: ["50%", "50%", "50%", "50%", "50%"],
+            }}
+            transition={{
+              duration: 2,
+              ease: "easeInOut",
+              times: [0, 0.2, 0.5, 0.8, 1],
+              repeat: Infinity,
+              repeatDelay: 1,
+            }}
             whileHover={{ scale: 1.25 }}
           >
             <MenuButton
