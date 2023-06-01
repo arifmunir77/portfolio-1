0b5f46624d3ce1d6ca0e828471737d56f93dd0c6
Dev-SardarAbdul <dev.sardarabdul@gmail.com>
2023-06-01 19:59:13 +0500

    modal updated

--- src/components/common/modal/workModal/index.tsx
+++ src/components/common/modal/workModal/index.tsx
@@ -56,65 +56,77 @@ const WorkModal = () => {
               transition={{ duration: 0.75, ease: "easeInOut" }}
               key={"container"}
             >
-              <SwiperWrapper
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 1, delay: 1 }}
+              <motion.div
+                initial={{ y: 50, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.5 }}
+                exit={{ y: 0, opacity: 0, transition: { duration: 0.4 } }}
               >
-                <Swiper
-                  // activeSlideKey='2'
-                  effect={"coverflow"}
-                  grabCursor={true}
-                  centeredSlides={true}
-                  autoplay={{ delay: 2000, disableOnInteraction: false }}
-                  slidesPerView={"auto"}
-                  coverflowEffect={{
-                    rotate: 0,
-                    stretch: 0,
-                    depth: 100,
-                    modifier: 2,
-                    slideShadows: true,
-                  }}
-                  // scrollbar={{draggable:true}}
-                  navigation={true}
-                  initialSlide={2}
-                  pagination={true}
-                  modules={[EffectCoverflow, Pagination, Navigation, Autoplay]}
-                  className="mySwiper"
+                <SwiperWrapper
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 1, delay: 1 }}
                 >
-                  <SwiperSlide>
-                    <Image fluid src={OurWorkImgOne} />
-                  </SwiperSlide>
+                  <Swiper
+                    // activeSlideKey='2'
+                    effect={"coverflow"}
+                    grabCursor={true}
+                    centeredSlides={true}
+                    autoplay={{ delay: 2000, disableOnInteraction: false }}
+                    slidesPerView={"auto"}
+                    coverflowEffect={{
+                      rotate: 0,
+                      stretch: 0,
+                      depth: 100,
+                      modifier: 2,
+                      slideShadows: true,
+                    }}
+                    // scrollbar={{draggable:true}}
+                    navigation={true}
+                    initialSlide={2}
+                    pagination={true}
+                    modules={[
+                      EffectCoverflow,
+                      Pagination,
+                      Navigation,
+                      Autoplay,
+                    ]}
+                    className="mySwiper"
+                  >
+                    <SwiperSlide>
+                      <Image fluid src={OurWorkImgOne} />
+                    </SwiperSlide>
 
-                  <SwiperSlide>
-                    <Image fluid src={OurWorkImgOne} />
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <Image fluid src={OurWorkImgOne} />
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <Image fluid src={OurWorkImgOne} />
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <Image fluid src={OurWorkImgOne} />
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <Image fluid src={OurWorkImgOne} />
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <Image fluid src={OurWorkImgOne} />
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <Image fluid src={OurWorkImgOne} />
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <Image fluid src={OurWorkImgOne} />
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <Image fluid src={OurWorkImgOne} />
-                  </SwiperSlide>
-                </Swiper>
-              </SwiperWrapper>
+                    <SwiperSlide>
+                      <Image fluid src={OurWorkImgOne} />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <Image fluid src={OurWorkImgOne} />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <Image fluid src={OurWorkImgOne} />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <Image fluid src={OurWorkImgOne} />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <Image fluid src={OurWorkImgOne} />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <Image fluid src={OurWorkImgOne} />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <Image fluid src={OurWorkImgOne} />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <Image fluid src={OurWorkImgOne} />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <Image fluid src={OurWorkImgOne} />
+                    </SwiperSlide>
+                  </Swiper>
+                </SwiperWrapper>
+              </motion.div>
             </WorkModalContainer>
           </Modal>
         )}
