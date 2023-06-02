8d3b04813239dc2ee965cb4261e905b484b3c6c3
Dev-SardarAbdul <dev.sardarabdul@gmail.com>
2023-06-03 00:45:24 +0500

    Contact nodal fixed

--- src/components/common/modal/contactModal/index.tsx
+++ src/components/common/modal/contactModal/index.tsx
@@ -52,7 +52,7 @@ const ContactModal = () => {
               <TextDiv
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
+                exit={{ opacity: 0, transition: { duration: 0.5, delay: 0.2 } }}
                 transition={{ duration: 1, delay: 1 }}
               >
                 <motion.div
