d2714cfde96459960b09babafee9f92e43d1f434
Dev-SardarAbdul <dev.sardarabdul@gmail.com>
2023-06-11 05:58:38 +0500

    border removed

--- src/components/pageChangeAnimation/element.ts
+++ src/components/pageChangeAnimation/element.ts
@@ -14,7 +14,6 @@ export const Page = styled(motion.div)`
   flex: 1;
   height: 100%;
   width: 100%;
-  border: 1px solid rgb(102, 252, 241, 0.3) !important;
   border-top: none;
   border-bottom: none;
 `;
