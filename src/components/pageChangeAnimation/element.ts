bc39a60ad3396c5251a98c5dabb53feb9606abba
Dev-SardarAbdul <dev.sardarabdul@gmail.com>
2023-06-17 04:07:55 +0500

    UI changes

--- src/components/pageChangeAnimation/element.ts
+++ src/components/pageChangeAnimation/element.ts
@@ -2,13 +2,13 @@ import styled from "styled-components";
 import { motion } from "framer-motion";
 
 export const PageAnimatorWrapper = styled.div`
-  height: 100vh;
   overflow: hidden !important;
   display: flex;
   justify-content: space-between;
   align-items: center;
   position: fixed;
   width: 100vw;
+  height: 100vh;
   top: 0;
   left: 0;
   z-index: 200;
@@ -22,4 +22,15 @@ export const Page = styled(motion.div)`
   width: 100%;
   border-top: none;
   border-bottom: none;
+  border: 1px solid rgb(102, 252, 241);
+  border-top: none;
+  border-bottom: none;
+  box-shadow: 0 0 20px rgb(102, 252, 241, 0.5);
+
+  &.no-border {
+    border-left: none !important;
+  }
+  &.no-right-border {
+    border-right: none !important;
+  }
 `;
--- src/components/pageChangeAnimation/index.tsx
+++ src/components/pageChangeAnimation/index.tsx
@@ -8,7 +8,11 @@ function PageAnimator() {
       <PageAnimatorWrapper key={"nav"}>
         {data.map((item, index) => (
           <Page
-            className={index % 2 === 1 && "odd"}
+            className={
+              (index % 2 === 1 && "odd") ||
+              (index === 0 && "no-border") ||
+              (index === 4 && "no-right-border")
+            }
             initial={index % 2 === 0 ? { y: -1000 } : { y: 1000 }}
             animate={{ y: 0 }}
             transition={{ delay: index * 0.1, duration: 1 }}
